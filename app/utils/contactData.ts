e18307acfc581ae3693680cd1f57c9fbf71e8e7f
goose-technology <goose.technology.it@gmail.com>
2025-10-06 13:28:41 +0200

    fix: add missing contactData.x to satisfy types

--- app/utils/contactData.ts
+++ app/utils/contactData.ts
@@ -4,5 +4,6 @@ export const contactData: contactDataType = {
   email: "jan.prysowecki@gmail.com",
   github: "https://github.com/goose-technology",
   linkedIn: "https://www.linkedin.com/in/jan-prysowecki?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+  x: "",
 
 };
